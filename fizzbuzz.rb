cdcbe9c3658654e3a0356f02f56a26ce7556a2b8
McFly1208 <car.barb.0904@gmail.com>
2023-12-06 14:13:38 -0600

    add fizz

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,4 @@
-[*1..100].each do |number|
-  result = number % 5 == 0 ? 'buzz' : number
+(1..100).each do |number|
+  result = number % 3 == 0 ? 'fizz' : (number % 5 == 0 ? 'buzz' : number)
   puts result
 end
